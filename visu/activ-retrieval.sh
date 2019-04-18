21473435d13d404cb3e15a53f62614ceb61a390a
Gvaihir <tnfepsilon@gmail.com>
2019-04-17 17:05:50 -0700

    Update activ-retrieval.sh

--- visu/activ-retrieval.sh
+++ visu/activ-retrieval.sh
@@ -9,6 +9,6 @@
 MODEL='/home/aogorodnikov/model_alex/checkpoint.pth.tar'
 EXP='/home/aogorodnikov/tmp'
 CONV=5
-DATA='/home/aogorodnikov/val/cropped_small_4'
+DATA='/home/aogorodnikov/val'
 
 python activ-retrieval.py --model ${MODEL} --exp ${EXP} --conv ${CONV} --data ${DATA}
