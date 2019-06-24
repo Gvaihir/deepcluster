ac7b6ffbd92e349b5b48f0b40f115e8045ea61c8
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2019-06-24 16:22:41 -0700

    fork v.0.2.1 Made classifier

---
+++ classif.sh
@@ -0,0 +1,24 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#!/bin/bash
+
+DATA="/home/aogorodnikov/classes/"
+MODEL="/home/aogorodnikov/model_alex_kmeans300/checkpoint.pth.tar"
+EXP="/home/aogorodnikov/kmeans300_linear_classif"
+PYTHON="/home/aogorodnikov/anaconda3/envs/imgSudoku/bin/python"
+EPOCHS=100
+BATCH=256
+
+
+mkdir -p ${EXP}
+
+${PYTHON} classif.py --model ${MODEL} --data ${DATA} \
+    --epochs ${EPOCHS} --batch_size ${BATCH} --lr 0.01 \
+    --make_test "True" --val_prob 0.2 --test_prob 0.1 \
+    --wd -7 --verbose --exp ${EXP}
+
+
