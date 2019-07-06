c9642787026a59a0394c1fb316fe06a11c015491
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2019-07-05 18:29:15 -0700

    fork v.0.3.2 Handling optimizer

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def main():
             model.load_state_dict(checkpoint['state_dict'])
             try:
                 optimizer.load_state_dict(checkpoint['optimizer'])
-            except: KeyError
+            except KeyError:
                 pass
 
             print("=> loaded checkpoint '{}' (epoch {})"
